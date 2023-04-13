4f79178ef103ddc38b7f2ea62fb18156f32d780c
kathalsol <alfarosolisk@gmail.com>
2023-04-13 10:32:05 -0600

    Arreglando includes del código

--- Labo2/src/labo2.c
+++ Labo2/src/labo2.c
@@ -1,2 +1,19 @@
+/* 
+   Universidad de Costa Rica
+   Laboratorio de Microcontroladores
+   Laboratorio 2 - Lavadora automática
+   Estudiantes:  
+    - Katharina Alfaro Solís B80251
+    - Mauricio Rodríguez Obando B96694
+*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt>
 #include <stdio.h>
-#include <stdlib.h>
+
+
+int main(void)
+{
+    
+}
