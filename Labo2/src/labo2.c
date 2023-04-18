141dc1e4d295abf389bb7407eb5f237389ee5eb0
kathalsol <alfarosolisk@gmail.com>
2023-04-17 21:25:08 -0600

    Cambios

--- Labo2/src/labo2.c
+++ Labo2/src/labo2.c
@@ -9,13 +9,15 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/interrupt>
+#include <avr/interrupt.h>
 #include <stdio.h>
 
 /* Se definen las varibles para las 3 interrupciones*/
 int interrupt_1 = 0;
 int interrupt_2 = 0;
 int interrupt_3 = 0;
+int counter, enable;
+int time_delay = 100;
 
 // Definición de estados
 #define waiting_user_input 0 // interrupcion
@@ -68,69 +70,75 @@ int main(void)
 
 	while(1){
 
-		case(waiting_user_input):
-			if ((interrupt_1 == 1) | (interrupt_2 == 1) | (interrupt_3 == 1)){
-				interrupt_1 == 0; interrupt_2 == 0; interrupt_3 == 0; // Vuelven a valor inicial
-				next_state == start_wash;
-			}
-			else{
-				next_state == waiting_user_input; // se sigue esperando la entrada del usuario
-			}
+		state = next_state;
+		switch(state){
+			case(waiting_user_input):
+				if ((interrupt_1 == 1) | (interrupt_2 == 1) | (interrupt_3 == 1)){
+					interrupt_1 == 0; interrupt_2 == 0; interrupt_3 == 0; // Vuelven a valor inicial
+					//next_state = start_wash;
+				}
+				else{
+					next_state == waiting_user_input; // se sigue esperando la entrada del usuario
+				}
 		
-		break;
+		    break;
+			/*
+			
+			case(start_wash): 
+				if(interrupt_1 == 1){ // Interrupt 1 = Baja
+					interrupt_1 =0;
+					green_light_charge(time_delay); // Indica que es un lavado con carga baja
+					// Indicar valores del tiempo para cada etapa ??
+					//next_state = suministro_agua;
 
-		case(start_wash): 
-			if(interrupt_1 == 1){ // Interrupt 1 = Baja
-				interrupt_1 ==0;
-				green_light_charge(delay) // Indica que es un lavado con carga baja
-				// Indicar valores del tiempo para cada etapa ??
-				next_state == suministro_agua;
+				}
 
-			}
+				else if(interrupt_2 == 1){ // Interrupt 2 = Media
+					interrupt_2 =0;
+					yellow_light_charge(time_delay); // Indica que es un lavado con carga media
+					//next_state = suministro_agua;
 
-			else if(interrupt_2 == 1){ // Interrupt 2 = Media
-				interrupt_2 ==0;
-				yellow_light_charge(delay) // Indica que es un lavado con carga media
-				next_state == suministro_agua;
+				}
 
-			}
+				else if(interrupt_3 == 1){ // Interrupt 2 = Media
+					interrupt_3 =0;
+					red_light_charge(time_delay); // Indica que es un lavado con carga media
+					//next_state = suministro_agua;
 
-			else if(interrupt_3 == 1){ // Interrupt 2 = Media
-				interrupt_3 ==0;
-				red_light_charge(delay) // Indica que es un lavado con carga media
-				next_state == suministro_agua;
+				}
 
-			}
+				else{
+					//next_state = start_wash;
+				}
+			break;
 
-			else{
-				next_state == start_wash;
-			}
-		break;
+			case(suministro_agua):
+				green_light(time_delay);
+				//next_state = lavar;
+			break;
 
-		case(suministro_agua):
-			green_light(delay);
-			next_state == lavar;
-		break;
+			case(lavar):
+				yellow_light(time_delay);
+				//next_state = enjuagar;
+			break;
 
-		case(lavar):
-			yellow_light(delay);
-			next_state == enjuagar;
-		break;
-		
-		case(centrifugar):
-			blue_light(delay);
-			next_state == checking;
-		break;
+			case(centrifugar):
+				blue_light(time_delay);
+				//next_state = checking;
+			break;
 
-		case(checking): 
-			// revisar si se paso por los 4 estados de lavado
-			// no se si sea necesario porque para llegar aqui tuvo que jaja
-		break;
-		
-		case(finish); 
-		break;
+			case(checking): 
+				// revisar si se paso por los 4 estados de lavado
+				// no se si sea necesario porque para llegar aqui tuvo que jaja
+			break;
+
+			case(finish):
+			break;
+			*/
+		}
+
 	}
-
+		
 }
 
 
