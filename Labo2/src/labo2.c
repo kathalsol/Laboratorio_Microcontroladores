3b70036900739077a37386d2d94cf59c11f9bd62
kathalsol <alfarosolisk@gmail.com>
2023-04-17 23:57:25 -0600

    Reeplanteo maquina estados

--- Labo2/src/labo2.c
+++ Labo2/src/labo2.c
@@ -13,33 +13,109 @@
 #include <stdio.h>
 
 /* Se definen las varibles para las 3 interrupciones*/
-int interrupt_1 = 0;
-int interrupt_2 = 0;
-int interrupt_3 = 0;
-int counter, enable;
+int counter, enable, state, wash_state;
 int time_delay = 100;
 
 // Definición de estados
-#define waiting_user_input 0 // interrupcion
 
-#define start_wash 0 // inicio ciclo de lavado, se define el patron de acuerdo
+#define waiting_user_input 0 
+
+#define low_load 1 //carga baja
+
+#define medium_load 2 // carga media
+
+#define high_load 3 // carga alta
+
+#define stop_wash 4
+
+#define start_wash 5 // inicio ciclo de lavado, se define el patron de acuerdo
 					 // al ciclo seleccionado, interrupcion del usuario
 
-#define suministro_agua 0 // primer paso del lavado, enciende led correspondiente 
+#define suministro_agua 6 // primer paso del lavado, enciende led correspondiente 
 						  // y pasa el tiempo en el display
 
-#define lavar 0 // segundo paso, enciende led correspondiente y pasa tiempo en el display
+#define lavar 7 // segundo paso, enciende led correspondiente y pasa tiempo en el display
 
-#define enjuagar 0 // tercer paso, enciende led correspondiente y pasa tiempo en el display
+#define enjuagar 8 // tercer paso, enciende led correspondiente y pasa tiempo en el display
 
-#define centrifugar 0 // cuarto paso, enciende led correspondiente y pasa tiempo en el display
+#define centrifugar 9 // cuarto paso, enciende led correspondiente y pasa tiempo en el display
 
-#define checking 0   // revisión de que haya pasado por los 4 estados de lavado:
-					 // tiene que encenderse los cuatro leds correspondientes
+#define finish 10  // fin del ciclo de lavado
 
-#define finish 0     // fin del lavado
 
 int main(void)
+{
+    setup();
+
+	char state = waiting_user_input;
+	char next_state = waiting_user_input;
+	char load=waiting_user_input;
+	while(1){
+		state = next_state;
+		switch(state){
+			case(start_wash):
+				switch(load){
+					case(low_load):
+						green_light_load(time_delay);
+						switch(wash_state){
+							case suministro_agua:
+								green_light(time_delay);
+								next_state = lavar;
+							case lavar:
+
+							case enjuagar:
+							
+							case centrifugar:
+						}
+					break;
+
+					case(medium_load):
+						yellow_light_load(time_delay);
+
+					
+					break;
+					case(high_load):
+						red_light_load(time_delay);
+
+					break;
+						
+				}
+
+			case(stop_wash):
+			break;
+		}
+
+	}
+		
+}
+
+
+ISR(PCINT11_vect) {
+    // Actualizar el estado de la lavadora en inicio
+    state = start_wash;
+}
+
+ISR(PCINT13_vect) {
+    // Actualizar el estado de la lavadora en función de la intensidad I_BAJA
+    state = low_load;
+}
+
+ISR(PCINT14_vect) {
+    // Actualizar el estado de la lavadora en función de la intensidad I_MEDIA
+    state = medium_load;
+}
+
+ISR(PCINT15_vect) {
+    // Actualizar el estado de la lavadora en función de la intensidad I_ALTA
+    state = high_load;
+}
+
+ISR(PCINT17_vect) {
+    // Actualizar estado para detener lavado
+    state = stop_wash;
+}
+
+void setup()
 {
     // Definición de salidas
 
@@ -52,95 +128,23 @@ int main(void)
 
 	/*
 	DDRD = Port D Data Direction Register 7 bits
-	Salidas puertos D1, D0
+	Salidas puertos D1 y D5, D0 como entrada
 	*/ 
-	DDRD = 0b0010011;
+	DDRD = 0b0010010;
 
 	/*
 	DDRA = Port A Data Register 3 bits
 	*/
-	DDRA = 0b010; // A1 como salida
+	DDRA = 0b011; // A1 como salida
 
-	/*Entradas con interrupciones corresponden a puertos D2, D3 Y D4
-	GIMSK = General Interrupt Mask Register */ 
-    GIMSK |= (1<<INT0)|(1<<INT1)|(1<<PCIE0); // interrupciones externas en D2 , D3 y D4 (BIT 7, 6 y 5)
+	/*Entradas con interrupciones corresponden a puertos D2, D3 Y D4*/
 
-	char state = waiting_user_input;
-	char next_state = waiting_user_input;
-
-	while(1){
-
-		state = next_state;
-		switch(state){
-			case(waiting_user_input):
-				if ((interrupt_1 == 1) | (interrupt_2 == 1) | (interrupt_3 == 1)){
-					interrupt_1 == 0; interrupt_2 == 0; interrupt_3 == 0; // Vuelven a valor inicial
-					//next_state = start_wash;
-				}
-				else{
-					next_state == waiting_user_input; // se sigue esperando la entrada del usuario
-				}
-		
-		    break;
-			/*
-			
-			case(start_wash): 
-				if(interrupt_1 == 1){ // Interrupt 1 = Baja
-					interrupt_1 =0;
-					green_light_charge(time_delay); // Indica que es un lavado con carga baja
-					// Indicar valores del tiempo para cada etapa ??
-					//next_state = suministro_agua;
-
-				}
-
-				else if(interrupt_2 == 1){ // Interrupt 2 = Media
-					interrupt_2 =0;
-					yellow_light_charge(time_delay); // Indica que es un lavado con carga media
-					//next_state = suministro_agua;
-
-				}
-
-				else if(interrupt_3 == 1){ // Interrupt 2 = Media
-					interrupt_3 =0;
-					red_light_charge(time_delay); // Indica que es un lavado con carga media
-					//next_state = suministro_agua;
-
-				}
-
-				else{
-					//next_state = start_wash;
-				}
-			break;
-
-			case(suministro_agua):
-				green_light(time_delay);
-				//next_state = lavar;
-			break;
-
-			case(lavar):
-				yellow_light(time_delay);
-				//next_state = enjuagar;
-			break;
-
-			case(centrifugar):
-				blue_light(time_delay);
-				//next_state = checking;
-			break;
-
-			case(checking): 
-				// revisar si se paso por los 4 estados de lavado
-				// no se si sea necesario porque para llegar aqui tuvo que jaja
-			break;
-
-			case(finish):
-			break;
-			*/
-		}
-
-	}
-		
+	PCMSK0 |= (1 << PCINT13) | (1 << PCINT14) | (1 << PCINT15); // Interrupciones de los niveles de carga
+	
+    PCMSK1 |= (1 << PCINT11) | (1 << PCINT17); // Boton inicio/pausa (mas prioridad)
+	
+    sei();
 }
-
 
 void delay(int time_delay){
 	TIMSK = 0b10;
@@ -191,21 +195,21 @@ void blue_light(int time_delay){
 el nivel de carga 
 */
 
-void green_light_charge(int time_delay){
+void green_light_load(int time_delay){
     PORTD = 0b0000001;
     delay(200);
     PORTD = 0b0000000; // Lo dejamos en bajo
 	delay(200);
 }
 
-void yellow_light_charge(int time_delay){
+void yellow_light_load(int time_delay){
     PORTD = 0b0000010;
     delay(200);
     PORTD = 0b0000000; // Lo dejamos en bajo
     delay(200);
 }
 
-void red_light_charge(int time_delay){
+void red_light_load(int time_delay){
     PORTA = 0b010;
     delay(200);
     PORTA = 0b000; // Lo dejamos en bajo
