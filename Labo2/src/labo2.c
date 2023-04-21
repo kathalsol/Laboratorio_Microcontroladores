8230cf0777ca8c379d29336681da1b728c576703
Mau1401 <mauricio.rodriguezobando@ucr.ac.cr>
2023-04-20 23:27:31 -0600

    Avance FSM,timer, estados de lavado

--- Labo2/src/labo2.c
+++ Labo2/src/labo2.c
@@ -13,21 +13,23 @@
 #include <stdio.h>
 
 //Definicion de pines
-#define STOP_BUTTON (1<<PD2)
-#define START_BUTTON (1<<PD3)
+#define RESET_BUTTON (1<<PD2)
+#define START_PAUSE_BUTTON (1<<PD3)
 #define MEDIUM_LOAD_BUTTON (1<<PD6)
 #define LOW_LOAD_BUTTON (1<<PB0)
 #define HIGH_LOAD_BUTTON (1<<PA0)
 #define LED_LOW_LOAD (1<<PD0)
 #define LED_MEDIUM_LOAD (1<<PD1)
 #define LED_HIGH_LOAD (1<<PA1)
-
+#define LED_START_PAUSE (1<<PB1)
+#define LED_STATE_WATER_SUPPLY (1<<PA2)
+#define LED_STATE_WASH (1<<PD4)
+#define LED_STATE_RISE (1<<PD5)
+#define LED_STATE_SPIN (1<<PB7)
 
 //Definicion de estados
 typedef enum {
 	STATE_IDEL,
-	STATE_START_WASH,
-	STATE_STOP_WASH,
 	STATE_WATER_SUPPLY,
 	STATE_WASH,
 	STATE_RISE,
@@ -38,41 +40,60 @@ typedef enum {
 
 // Se definen las varibles globales
 volatile state_t state = STATE_IDEL;
+volatile state_t next_state = STATE_IDEL;
 volatile uint8_t load = 0;
+volatile uint8_t compare_value = 0;
 
 //Declaracion de funciones
 void setup();
+void setup_timer();
 void delay_ms(uint16_t ms);
+void fsm();
+void water_supply(uint8_t load);
+void wash(uint8_t load);
+void rise(uint8_t load);
+void spin(uint8_t load);
+
+
+
 
 
 int main(void)
 {
     setup();
+	setup_timer();
+	// Habilitar interrupciones globales
+    sei();
 	while(1){
-
+		fsm();
 	}
 	return 0;
 }
 
-ISR(INT0_vect) //Boton pausa
+ISR(INT0_vect) //Boton reset
 {
 	//state = stop_wash;
-	if(PIND & STOP_BUTTON){
+	if(PIND & RESET_BUTTON){
 		state = STATE_IDEL;
-	}
-	else{
-
 	}
 }
 
-ISR(INT1_vect) //Boton start
+ISR(INT1_vect) //Boton start/pause
 {
-	if(PIND & START_BUTTON){
-		state = STATE_START_WASH;
+	if(PIND & START_PAUSE_BUTTON){
+		//Si el timer esta activo se detiene
+		if (TIMSK & (1<<TOIE0)){
+			TIMSK &= ~(1<<TOIE0);
+			PORTB &= ~LED_START_PAUSE;
+		}
+		//Si esta detenido lo inciamos
+		else{
+			TIMSK |= (1<<TOIE0);
+			PORTB |= LED_START_PAUSE;
+		}
+		
 	}
-	else {
 
-	}
 }
 ISR(PCINT0_vect)//Interupcion carga baja 
 {
@@ -105,36 +126,30 @@ ISR(PCINT1_vect) //Interupcion carga alta
 	else {
 		PORTA &= ~LED_HIGH_LOAD;
 	}
-} 
+}
 
+ISR(TIMER0_COMPA_vect) {
+  // Hacer LED display cada vez que se produzca la interrupción
+	
+
+}
 
 
 
 void setup()
-/* Definición de entradas y salidas
-   - DDRB: Port B Data Direction Register 8 bits
-	Configuracion del puerto B: Todos como salidas
-	Puertos B0,B1,B2 y B3 *(LEDS)y los leds B7 B6 B5 B4 (Para el decodificador)
-   - DDRD: Port D Data Direction Register 7 bits
-	Salidas puertos D1 y D5, D0 como entrada
-   - DDRA: Port A Data Register 3 bits
-	A1 y A0 como salida
-   - PCMSK0 y PCMSK1 : Pin change mask register 0 and 1
-	Utilizados para definir en que puertos I/O estan las interrupciones
-	D2, D3 Y D4, además de D0 y D6
-*/
 {
 
 	//Configuras los pines como entrada
 	DDRB &= ~LOW_LOAD_BUTTON;
 	DDRA &= ~HIGH_LOAD_BUTTON;
-	DDRD &= ~(MEDIUM_LOAD_BUTTON | STOP_BUTTON | START_BUTTON);
+	DDRD &= ~(MEDIUM_LOAD_BUTTON | RESET_BUTTON | START_PAUSE_BUTTON);
 	//Configuras los pines como salida
-	DDRD |= (LED_MEDIUM_LOAD | LED_LOW_LOAD);
-	DDRA |=	LED_HIGH_LOAD;
+	DDRD |= (LED_MEDIUM_LOAD | LED_LOW_LOAD | LED_STATE_WASH | LED_STATE_RISE);
+	DDRB |= (LED_START_PAUSE | LED_STATE_SPIN);
+	DDRA |=	(LED_HIGH_LOAD | LED_STATE_WATER_SUPPLY); //revisar como poner como salida pin A2
 
 	// Habilitar pull-up en botones
-    PORTD |= (START_BUTTON | STOP_BUTTON | MEDIUM_LOAD_BUTTON);
+    PORTD |= (START_PAUSE_BUTTON | RESET_BUTTON | MEDIUM_LOAD_BUTTON);
 	PORTB |= LOW_LOAD_BUTTON; 
 	PORTA |= HIGH_LOAD_BUTTON;
 
@@ -147,16 +162,53 @@ void setup()
 
 	//Configuracion GIMSK para activar interupciones
 	GIMSK |= ((1<<PCIE0) | (1<<PCIE1) | (1<<PCIE2)); //Interupciones de los botones de carga
-	GIMSK |= (1 << INT0) | (1 << INT1); // Interrupcion de inicio y final
+	GIMSK |= ((1 << INT0) | (1 << INT1)); // Interrupcion de start/pause y reset
 
 	//MCUCR |= (1<<ISC01) | (1<<ISC00); // LA INTERRUPCION SE DETECTARA EN LOS FLANCOS POSITIVOS.
 	
-	
-	// Habilitar interrupciones globales
-    sei();
-}
+};
+void setup_timer()
+{
+	// Configurar el prescaler a 64
+	TCCR0B |= (1 << CS01) | (1 << CS00);
 
-void delay_ms(uint16_t ms) {
+	// Definir el valor del comparador
+	OCR0A = compare_value;
+
+	// Habilitar la interrupción de comparación A
+  	TIMSK |= (1 << OCIE0A);
+};
+void fsm()
+{	
+	switch (state)
+	{
+	case STATE_IDEL:
+
+		break;	
+	case STATE_WATER_SUPPLY:
+		water_supply(load);
+
+		break;
+	case STATE_WASH:
+		wash(load);
+
+		break;
+	case STATE_RISE:
+		rise(load);
+
+		break;
+	case STATE_SPIN:
+		spin(load);
+
+		break;
+	case STATE_FINISH:
+		
+		break;
+	}
+	state = next_state;
+};
+void delay_ms(uint16_t ms) 
+{
     uint16_t count = 256 - ((F_CPU / 64) / 1000);
 
     while (ms > 0) {
@@ -171,7 +223,88 @@ void delay_ms(uint16_t ms) {
 
         ms--;
     }
-}
+};
+void water_supply(uint8_t load)
+{	
+	PORTA |= LED_STATE_WATER_SUPPLY;
+	PORTD &= ~(LED_STATE_WASH | LED_STATE_RISE );
+	PORTB &= ~LED_STATE_SPIN;
+	next_state = STATE_WASH;
+	if (load == 0){
+		compare_value = 1;
+	}
+	else if (load == 1){
+		compare_value = 1;
+	}
+	else if (load == 2){
+		compare_value = 1;
+	}
+	else if (load == 3){
+		compare_value = 1;
+	}
+};
+
+void wash(uint8_t load)
+{	
+	PORTD |= LED_STATE_WASH;
+	PORTD &= ~LED_STATE_RISE ;
+	PORTA &= ~LED_STATE_WATER_SUPPLY;
+	PORTB &= ~LED_STATE_SPIN;
+	next_state = STATE_RISE;
+	if (load == 0){
+		compare_value = 1;
+	}
+	else if (load == 1){
+		compare_value = 1;
+	}
+	else if (load == 2){
+		compare_value = 1;
+	}
+	else if (load == 3){
+		compare_value = 1;
+	}
+};
+
+void rise(uint8_t load)
+{	
+	PORTD |= LED_STATE_RISE;
+	PORTA &= ~LED_STATE_WATER_SUPPLY;
+	PORTD &= ~LED_STATE_WASH;
+	PORTB &= ~LED_STATE_SPIN;
+	next_state = STATE_SPIN;
+	if (load == 0){
+		compare_value = 1;
+	}
+	else if (load == 1){
+		compare_value = 1;
+	}
+	else if (load == 2){
+		compare_value = 1;
+	}
+	else if (load == 3){
+		compare_value = 1;
+	}
+};
+void spin(uint8_t load)
+{	
+	PORTB |= LED_STATE_SPIN;
+	PORTD &= ~(LED_STATE_WASH | LED_STATE_RISE);
+	PORTA &= ~LED_STATE_WATER_SUPPLY;
+	next_state = STATE_FINISH;
+	if (load == 0){
+		compare_value = 1;
+	}
+	else if (load == 1){
+		compare_value = 1;
+	}
+	else if (load == 2){
+		compare_value = 1;
+	}
+	else if (load == 3){
+		compare_value = 1;
+	}
+};
+
 
 /*
 void led_display(int valor, int display)
